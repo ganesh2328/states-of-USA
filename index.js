290e028899e1be4f32df5ff2b41325fb40bcbfd3
gollavilli ganesh kumar <84216653+ganesh2328@users.noreply.github.com>
2021-07-27 22:58:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+const endpoint =
+  "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+let cities = [];
+useIt();
+
+async function soFetch(input, settings = {}) {
+  const response = await fetch(input, {});
+  if (response.status === 200) {
+    console.log(response);
+    return response.json();
+  }
+}
+async function useIt() {
+  const items = await soFetch(endpoint);
+  cities.push(...items);
+  console.log(cities);
+  console.log(cities.length);
+}
+function findMatches(wordToMatch, cities) {
+  return cities.filter(place => {
+    // here we need to figure out if the city or state matches what was searched
+    const regex = new RegExp(wordToMatch, "gi");
+    return place.city.match(regex) || place.state.match(regex);
+  });
+}
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function displayMatches() {
+  const matchArray = findMatches(this.value, cities);
+  const html = matchArray
+    .map(place => {
+      const regex = new RegExp(this.value, "gi");
+      const cityName = place.city.replace(
+        regex,
+        `<span class="hl">${this.value}</span>`
+      );
+      const stateName = place.state.replace(
+        regex,
+        `<span class="hl">${this.value}</span>`
+      );
+      return `<li><span class="name">${cityName}, ${stateName}</span><span class="population">${numberWithCommas(
+        place.population
+      )}</span></li> `;
+    })
+    .join("");
+  suggestions.innerHTML = html;
+}
+const searchInput = document.querySelector(".search");
+const suggestions = document.querySelector(".suggestions");
+searchInput.addEventListener("change", displayMatches);
+searchInput.addEventListener("keyup", displayMatches);
